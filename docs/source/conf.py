ae4974705e24beffb866f040c81dd7d58ab1f426
Carlos Vieira <3831408+caduvieira@users.noreply.github.com>
2022-08-08 09:43:55 -0300

    chore: remove dados do template
    Remove imports do template od sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SUPER.GOVBR'
+copyright = '2021, Super GOVBR'
+author = 'Super'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_govbr_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
